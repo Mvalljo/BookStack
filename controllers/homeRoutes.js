a2ad76a02b7b495d857b029ecaa1fcd0b832ccf6
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-11-06 13:27:37 -0500

    changed file name and added sign up and log in routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signUp", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signUp");
+});
+
+module.exports = router;
