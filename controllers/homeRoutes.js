79e08d9d2ccb59c4485d030977b7fb48805edffe
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-11-07 23:58:29 -0600

    created home route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,6 +11,10 @@ router.get("/", (req, res) => {
   res.render("login");
 });
 
+router.get("/home", (req, res) => {
+  res.render("home");
+});
+
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
