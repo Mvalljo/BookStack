00fd83988e8f8f47e7d9d6691e94ca546de2e439
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-11-07 19:15:14 -0600

    coded idex.js model

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const User = require("./user");
+const Post = require("./Post");
+const Books = require("./Books");
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+  through: {
+    model: Books,
+    unique: false,
+  },
+  as: "read_books",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  through: {
+    model: Books,
+    unique: false,
+  },
+  as: "user_post",
+});
+
+module.exports = { User, Post, Books };
